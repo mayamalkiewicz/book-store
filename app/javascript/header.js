af1138614dc993d6b34798379c603049457dd28d
Maja <maya.malkiewicz@llinformatics.com>
2022-12-14 17:20:57 +0100

    Fix CSS styling

---
+++ app/javascript/header.js
@@ -0,0 +1,12 @@
+
+    
+    window.addEventListener('scroll',(e)=>{
+        const nav = document.querySelector('.header-container');
+            if(window.pageYOffset>0){
+                nav.classList.add("add-shadow");
+            }else{
+                nav.classList.remove("add-shadow");
+            }
+        }
+    );
+   
