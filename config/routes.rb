4de77aba5b16c7ad0598597be5132970d79d2582
Maja <maya.malkiewicz@llinformatics.com>
2022-12-14 16:14:30 +0100

    Clear not using articles rout, views etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
   root 'home#index'
   #root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  #get "/articles", to: "articles#index"
-  #get "/articles/:id", to: "articles#show"
-  #get "articles/about"
+
 
   get 'home/list'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_08_120546) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
