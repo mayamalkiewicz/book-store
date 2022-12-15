caf198aed0bc8a786cae7a9f808019734a4a7380
Maja <maya.malkiewicz@llinformatics.com>
2022-12-15 17:56:49 +0100

    Add .gitnore file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'home#index'
-  #root "articles#index"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-
-
   get 'home/list'
 
   
