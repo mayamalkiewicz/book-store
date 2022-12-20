473656c81765ce66e73bd0ef0c5ce01f9ab6a421
Maja <maya.malkiewicz@llinformatics.com>
2022-12-20 17:10:56 +0100

    Delete db/development.sqlite3, fix empty lines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
   end
-
   def list
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   # Defines the root path route ("/")
   root 'home#index'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/list'
-
 end
